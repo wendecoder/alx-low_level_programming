b541ee67583a466cfb10e5c7a4d87bc531301e30
wendecoder <wenonezra258@gmail.com>
2022-03-11 09:02:41 -0800

    control structure

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: prints all single digit numbers of base 10
+ * starting from 0, followed by a new line.
+ * Return: 0
+ */
+int main(void)
+{
+	char d, c;
+	d = '0';
+	c = 'a';
+	while (d <= '9')
+	{
+		putchar(d);
+		d++;
+	}
+
+	while (c <= 'f')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
+
+
