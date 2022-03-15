f75957672d3de57921e23b3ab448f3c8ae525869
wendecoder <wenonezra258@gmail.com>
2022-03-15 08:54:10 -0700

    function and nested loop

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char c[8] = "_putchar";
+	int i = 0;
+	while(i < 8)
+	{
+	_putchar(c[i]);
+	i++;
+	}
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ *@n: number being tested
+ * Return: Always 0.
+ */
+
+int print_last_digit(int n)
+{
+	n = n % 10;
+
+	if (n > 0)
+		_putchar(n + '0');
+	else if (n == 0)
+		_putchar(n + '0');
+	else if (n < 0)
+	{
+		n = -n;
+		_putchar(n + '0');
+	}
+		return (n);
+}
