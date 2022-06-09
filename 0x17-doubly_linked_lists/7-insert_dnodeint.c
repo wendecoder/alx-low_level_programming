de6c4b654a5608d8112cd85c66bd8f1cfde41220
wendecoder <wenonezra258@gmail.com>
2022-06-09 11:23:41 -0700

    doubly linked list in c

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,55 @@
+#include "lists.h"
+/**
+  * insert_dnodeint_at_index - inserts new node at a given position.
+  * @h: double pointer to head node of d-list.
+  * @idx: position to add new node.
+  * @n: data to add to new node.
+  *
+  * Return: address of new node or NULL
+  * om failure.
+  */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new_Node;
+	dlistint_t *pro;
+	unsigned int index = 0;
+	unsigned int i = 0;
+
+	if (idx == 0)
+	{
+		new_Node = add_dnodeint(h, n);
+		return (new_Node);
+	}
+	pro = *h;
+	while (pro != NULL)
+	{
+		pro = pro->next;
+		i++;
+	}
+	if (idx == i)
+	{
+		new_Node = add_dnodeint_end(h, n);
+		return (new_Node);
+	}
+	new_Node = (dlistint_t *)malloc(sizeof(dlistint_t));
+	if (new_Node == NULL)
+		return (NULL);
+	new_Node->n = n;
+	pro = *h;
+	while (index < idx)
+	{
+		pro = pro->next;
+		index++;
+		if (pro == NULL)
+		{
+			free(new_Node);
+			return (NULL);
+		}
+	}
+	new_Node->prev = pro->prev;
+	new_Node->next = pro;
+	pro->prev->next = new_Node;
+	pro->prev = new_Node;
+	return (new_Node);
+}
+}
---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,59 @@
+#include "lists.h"
+/**
+  * dlistint_len - returns number of elements in doubly linked list.
+  * @h: ponter to head node of list.
+  *
+  * Return: number of elements in list.
+  */
+size_t dlistint_len(const dlistint_t *h)
+{
+	/* i is counter var */
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
+
+/**
+  * delete_nodeint_at_index - deletes node of a doubly linked list
+  * at a speified index.
+  * @head: pointer to head of d-list.
+  * @index: position of node to * 0 ;-)
+  *
+  * Return: 1 on success, -1 on failure.
+  */
+int delete_nodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	unsigned int idx = 0;
+	dlistint_t *pro;
+
+	pro = *head;
+	if (*head == NULL || index > dlistint_len(pro))
+		return (-1);
+	if (index == 0 && pro->next)
+	{
+		pro->next->prev = NULL;
+		*head = pro->next;
+		free(pro);
+		return (1);
+	}
+	else if (index == 0 && pro)
+	{
+		free(pro);
+		*head = NULL;
+		return (1);
+	}
+	while (idx < index)
+	{
+		pro = pro->next;
+		idx++;
+	}
+	pro->prev->next = pro->next;
+	if (pro->next)
+		pro->next->prev = pro->prev;
+	free(pro);
+	return (1);
