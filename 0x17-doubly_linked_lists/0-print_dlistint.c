ee5e106b7f7ca657d5f733c198885beb4b9edae1
wendecoder <wenonezra258@gmail.com>
2022-06-09 08:11:48 -0700

    ndc

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_list = it prints every elements of 
+ * the linked list.
+ */
+
+size_t print_dlistint(const dlistint_t *h){
+	const dlistint_t *pro = h;
+	size_t i = 0;
+	while (pro != NULL)
+	{
+		printf("%d\n", pro->n);
+		pro = pro->next;
+		i++;
+	}
+	return (i);
+}
