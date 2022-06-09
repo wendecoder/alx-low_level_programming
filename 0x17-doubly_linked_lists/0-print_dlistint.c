c4a96fb7ba22b393a61aa0e60e2019c14fe67a26
wendecoder <wenonezra258@gmail.com>
2022-06-09 08:23:59 -0700

    cdencfj

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,9 +10,10 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	const dlistint_t *pro = h;
+	const dlistint_t *pro;
 	size_t i = 0;
 
+	pro = h;
 	while (pro != NULL)
 	{
 		printf("%d\n", pro->n);
